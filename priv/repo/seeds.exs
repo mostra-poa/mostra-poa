d83ee3efc8ce97e4c5f7d7b4751658a2ffce371b
João Thallis <joaothallis@icloud.com>
2023-04-19 20:28:51 -0300

    Add in router

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,26 +12,56 @@
 
 alias MostraPoa.{Repo, Blog.Post}
 
-Repo.insert! %Post{
+Repo.insert!(%Post{
   title: "Hello world",
   body: "This is my first post",
   visible: true
-}
+})
 
-Repo.insert! %Post{
+Repo.insert!(%Post{
   title: "Hello world",
   body: "This is my second post",
   visible: true
-}
+})
 
-Repo.insert! %Post{
+Repo.insert!(%Post{
   title: "Hello world",
   body: "This is my third post",
   visible: true
-}
+})
 
-Repo.insert! %Post{
+Repo.insert!(%Post{
   title: "Hello world",
   body: "This is my first invisble post",
   visible: false
-}
+})
+
+Repo.insert!(%MostraPoa.Sessions.Session{
+  name: "Why Use Elixir",
+  location: "Building B",
+  start: ~N[2023-04-19 23:29:59] ,
+  visible: false
+})
+
+Repo.insert!(%MostraPoa.Sessions.Session{
+  name: "Hello world",
+  location: "Building A",
+  start: ~N[2023-04-19 15:29:59] ,
+  visible: true
+})
+
+Repo.insert!(%MostraPoa.Sessions.Session{
+  name: "Bah",
+  location: "Building B",
+  start: ~N[2023-04-19 12:29:59] ,
+  end: ~N[2023-04-19 16:29:59] ,
+  visible: true
+})
+
+Repo.insert!(%MostraPoa.Sessions.Session{
+  name: "Xabla",
+  location: "Building C",
+  start: ~N[2023-04-18 12:29:59] ,
+  end: ~N[2023-04-18 16:29:59] ,
+  visible: true
+})
--- test/mostra_poa_web/controllers/session_controller_test.exs
+++ test/mostra_poa_web/controllers/session_controller_test.exs
@@ -1,26 +1,6 @@
 defmodule MostraPoaWeb.SessionControllerTest do
   use MostraPoaWeb.ConnCase
 
-  import MostraPoa.SessionsFixtures
-
-  alias MostraPoa.Sessions.Session
-
-  @create_attrs %{
-    end: ~N[2023-04-18 23:04:00],
-    location: "some location",
-    name: "some name",
-    start: ~N[2023-04-18 23:04:00],
-    visible: true
-  }
-  @update_attrs %{
-    end: ~N[2023-04-19 23:04:00],
-    location: "some updated location",
-    name: "some updated name",
-    start: ~N[2023-04-19 23:04:00],
-    visible: false
-  }
-  @invalid_attrs %{end: nil, location: nil, name: nil, start: nil, visible: nil}
-
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
@@ -32,69 +12,4 @@ defmodule MostraPoaWeb.SessionControllerTest do
     end
   end
 
-  describe "create session" do
-    test "renders session when data is valid", %{conn: conn} do
-      conn = post(conn, ~p"/api/sessions", session: @create_attrs)
-      assert %{"id" => id} = json_response(conn, 201)["data"]
-
-      conn = get(conn, ~p"/api/sessions/#{id}")
-
-      assert %{
-               "id" => ^id,
-               "end" => "2023-04-18T23:04:00",
-               "location" => "some location",
-               "name" => "some name",
-               "start" => "2023-04-18T23:04:00",
-               "visible" => true
-             } = json_response(conn, 200)["data"]
-    end
-
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, ~p"/api/sessions", session: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
-
-  describe "update session" do
-    setup [:create_session]
-
-    test "renders session when data is valid", %{conn: conn, session: %Session{id: id} = session} do
-      conn = put(conn, ~p"/api/sessions/#{session}", session: @update_attrs)
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
-
-      conn = get(conn, ~p"/api/sessions/#{id}")
-
-      assert %{
-               "id" => ^id,
-               "end" => "2023-04-19T23:04:00",
-               "location" => "some updated location",
-               "name" => "some updated name",
-               "start" => "2023-04-19T23:04:00",
-               "visible" => false
-             } = json_response(conn, 200)["data"]
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, session: session} do
-      conn = put(conn, ~p"/api/sessions/#{session}", session: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
-
-  describe "delete session" do
-    setup [:create_session]
-
-    test "deletes chosen session", %{conn: conn, session: session} do
-      conn = delete(conn, ~p"/api/sessions/#{session}")
-      assert response(conn, 204)
-
-      assert_error_sent 404, fn ->
-        get(conn, ~p"/api/sessions/#{session}")
-      end
-    end
-  end
-
-  defp create_session(_) do
-    session = session_fixture()
-    %{session: session}
-  end
 end
