00b649895bb04ced94e3c0a2557e762cef2c3b97
João Thallis <joaothallis@icloud.com>
2023-06-19 00:49:24 -0300

    Fix mui installation

--- frontend/src/timeline/TimelinePage.tsx
+++ frontend/src/timeline/TimelinePage.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -8,49 +7,37 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
+  activity: string,
+  date_or_deadline: string
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { activity, date_or_deadline };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Realização da 12ª Mostra de Ensino, Extensão e Pesquisa', '24 e 25 de outubro de 2023'),
 ];
 
 export default function TimelinePage() {
   return (
     <TableContainer component={Paper}>
+      <h1>Cronograma</h1>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Atividade</TableCell>
+            <TableCell align="right">Datas (prazos)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
-              key={row.name}
+              key={row.activity}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.activity}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.date_or_deadline}</TableCell>
             </TableRow>
           ))}
         </TableBody>
