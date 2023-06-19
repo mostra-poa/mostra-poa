cf09179c1f8ef52b1ba2d4d8fcbb3c33961048e8
João Thallis <joaothallis@icloud.com>
2023-06-19 01:14:47 -0300

    Import fonts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,11 @@ import SessionPage from "./session/SessionPage";
 import HomePage from "./home/HomePage";
 import TimelinePage from "./timeline/TimelinePage";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const style = { display: "flex", gap: "8px", };
 
 function App() {
